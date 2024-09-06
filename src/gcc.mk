e9f5fffc43a88c09eb2d6427b0fe222f7f9bf957
Vedran Novakovic <venovako@venovako.eu>
2024-09-07 01:32:42 +0200

    further updates

--- src/gcc.mk
+++ src/gcc.mk
@@ -1,7 +1,7 @@
 ARFLAGS=rsv
 CC=$(COMPILER_PREFIX)gcc$(COMPILER_SUFFIX)
 ifdef NDEBUG
-CFLAGS=-DNDEBUG=$(NDEBUG) -O$(NDEBUG) -fno-math-errno -fvect-cost-model=unlimited
+CFLAGS=-DNDEBUG=$(NDEBUG) -O$(NDEBUG) -fno-math-errno
 else # DEBUG
 CFLAGS=-Og -ggdb3 -ftrapv
 endif # ?NDEBUG
@@ -11,7 +11,7 @@ CFLAGS += -D_GNU_SOURCE
 else # !Linux
 CFLAGS += -m64
 endif # ?Linux
-CFLAGS += -std=gnu$(shell if [ `$(CC) -dumpversion | cut -f1 -d.` -ge 14 ]; then echo 23; else echo 18; fi) -fPIC -fexceptions -fasynchronous-unwind-tables -ffp-contract=fast -fno-omit-frame-pointer -fopenmp -march=native
+CFLAGS += -std=gnu$(shell if [ `$(CC) -dumpversion | cut -f1 -d.` -ge 14 ]; then echo 23; else echo 18; fi) -fPIC -fexceptions -fasynchronous-unwind-tables -ffp-contract=fast -fopenmp -fno-omit-frame-pointer -fvect-cost-model=unlimited -march=native
 LDFLAGS=-rdynamic -static-libgcc
 ifeq ($(findstring BSD,$(OS)),BSD)
 LDFLAGS += -lexecinfo
--- src/icx.mk
+++ src/icx.mk
@@ -2,11 +2,11 @@ AR=xiar
 ARFLAGS=-qnoipo -lib rsv
 CC=$(COMPILER_PREFIX)icx$(COMPILER_SUFFIX)
 ifdef NDEBUG
-CFLAGS=-DNDEBUG=$(NDEBUG) -O$(NDEBUG) -fno-math-errno -inline-level=2 -qopt-report=3 -vec-threshold0
+CFLAGS=-DNDEBUG=$(NDEBUG) -O$(NDEBUG) -fno-math-errno -inline-level=2 -qopt-report=3
 else # DEBUG
 CFLAGS=-O0 -g -debug extended -debug inline-debug-info -debug pubnames -debug parallel -ftrapv
 endif # ?NDEBUG
-CFLAGS += -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -std=gnu18 -fPIC -fexceptions -fasynchronous-unwind-tables -fp-model=precise -fp-speculation=safe -fprotect-parens -fma -no-ftz -qopenmp -fno-omit-frame-pointer -traceback -xHost
+CFLAGS += -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -std=gnu18 -fPIC -fexceptions -fasynchronous-unwind-tables -fp-model=precise -fp-speculation=safe -fimf-precision=high -fprotect-parens -fma -no-ftz -qopenmp -fno-omit-frame-pointer -mprefer-vector-width=512 -traceback -vec-threshold0 -xHost
 LDFLAGS=-rdynamic -static-libgcc -ldl
 ifndef QUADMATH
 QUADMATH=$(shell gcc -print-file-name=libquadmath.a)
